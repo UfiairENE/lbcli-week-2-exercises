163aad569b1d98309f7732cf36dad2c9650fe892
UfiairENE <irenecufia@gmail.com>
2026-03-24 14:00:00 +0300

    feat: complete Week 2 exercises

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+#!/bin/bash
+
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet -named getnewaddress address_type="p2sh-segwit")
+bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS" > /dev/null
+echo "$ADDRESS"
--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,5 @@
 # List the current UTXOs in your wallet.
+
+#!/bin/bash
+
+bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent
